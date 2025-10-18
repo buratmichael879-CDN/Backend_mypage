46c0f37c49542bc8fc1fbb2651489d77cd79d9f8
buratmichael879-CDN <buratmichael879@gmail.com>
2025-10-18 07:20:43 +0200

    Update luftdicht.js

--- luftdicht.js
+++ luftdicht.js
@@ -110,8 +110,8 @@ function sanitizeAgainstXSS(input) {
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;'")
-      .replace(/'/g, '&#x27;'")
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#x27;')
       .replace(/\//g, '&#x2F;')
       .replace(/`/g, '&#x60;')
       .replace(/\$\{/g, '&#36;&#123;');
